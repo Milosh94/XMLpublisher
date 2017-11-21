820c6aadb77aea6b5e3315a9468573727b9a2195
milos01 <milosa942@gmail.com>
2017-11-21 14:43:38 +0100

    Project setup

---
+++ src/main/java/com/mmm/xmlpublisher/XmlpublisherApplication.java
@@ -0,0 +1,12 @@
+package com.mmm.xmlpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlpublisherApplication.class, args);
+	}
+}
